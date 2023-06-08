312f6bc201aa2880d8c6f240b9da0cf30459a234
rockspapers <135973264+rockspapers@users.noreply.github.com>
2023-06-08 15:02:38 +0200

    Add files via upload

--- static/scripts/scroll.js
+++ static/scripts/scroll.js
@@ -1,2 +1,7 @@
 var scrolable = document.querySelector("#nosee");
 scrolable.scrollTop = scrolable.scrollHeight;
+
+function playaudio(){
+    var audio = document.querySelector("#acontrol");
+    audio.play();
+}
